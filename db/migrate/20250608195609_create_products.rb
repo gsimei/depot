5a2c9f3c60a736f1a40b11118a99cf0510d92c3b
George Simei <georgesimei@gmail.com>
2025-06-08 21:58:18 +0200

    Fixed the typo in the migration file.

--- db/migrate/20250608195609_create_products.rb
+++ db/migrate/20250608195609_create_products.rb
@@ -1,7 +1,7 @@
 class CreateProducts < ActiveRecord::Migration[8.0]
   def change
     create_table :products do |t|
-      t.string : title
+      t.string :title
       t.text :description
       t.decimal :price
 
